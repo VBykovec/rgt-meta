a327bca9f893f7966422bb550f5842e667a48777
Vitaliy Bykovec <vbykovec@gmail.com>
2013-05-21 00:57:16 +0300

    after small refactoring

--- knowledge.rb
+++ knowledge.rb
@@ -1,42 +1,32 @@
-def attribute(name, value = nil, &block)
-    if name.is_a?(Symbol)|| name.is_a?(String)
-        define_method name do
-          if instance_variable_defined? "@#{name}"
-              instance_variable_get "@#{name}" 
-          else
-            if block
-                instance_eval &block
-            else
-                value
-            end
-          end
-        end
-        define_method "#{name}=" do |val|
-          instance_variable_set "@#{name}", val
-        end
-        define_method "#{name}?" do
-          !instance_eval("#{name}").nil?
-        end
+def attribute(attr_name, attr_value = nil, &block_given)
+  if attr_name.is_a?(Symbol) || attr_name.is_a?(String)
+    attribute_real attr_name, attr_value, &block_given
+  end
+  if attr_name.is_a? Hash
+    attr_name.each do |key,val|
+      attribute_real key,val, &block_given
     end
-    if name.is_a? Hash
-        name.each do |key,val|
-            define_method key do
-                if instance_variable_defined? "@#{key}"
-                    instance_variable_get "@#{key}" 
-                else
-                    if block
-                        instance_eval &block
-                    else
-                        val
-                    end
-                end
-            end
-            define_method "#{key}=" do |valr|
-              instance_variable_set "@#{key}", valr
-            end
-            define_method "#{key}?" do
-              !instance_eval("#{key}").nil?
-            end
-        end
+  end
+end
+
+def attribute_real(name, value = nil, &block)
+  define_method name do
+    if instance_variable_defined? "@#{name}"
+      instance_variable_get "@#{name}" 
+    else
+      if block
+        instance_eval &block
+      else
+        value
+      end
     end
+  end
+
+  define_method "#{name}=" do |val|
+    instance_variable_set "@#{name}", val
+  end
+
+  define_method "#{name}?" do
+    !instance_eval("#{name}").nil?
+  end
 end
