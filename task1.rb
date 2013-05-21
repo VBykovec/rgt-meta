680af06ec86a07cf3b71749cc86dc75573f13abd
Vitaliy Bykovec <vbykovec@gmail.com>
2013-05-21 18:22:34 +0300

    task1

--- task1.rb
+++ task1.rb
@@ -1,4 +1,3 @@
-
 module Aop
   def self.included(mod)
     mod.extend RAop
@@ -7,38 +6,59 @@ module Aop
     def self.extended(base)
       base.instance_eval do
         def method_added(name)
-          return if !@methods_before.keys.include? && @methods_after.keys.include? name 
-          class_eval do
-            unless method_defined?(:)
-              define_method(:) do
+          return if !@methods_before.keys.include?(name.to_s) && !@methods_after.keys.include?(name.to_s)
+            unless method_defined?("original_#{name}")
+              alias_method "original_#{name}", name
+              define_method("#{name}_new") do
+                methods_before = self.class.instance_variable_get '@methods_before'
+                methods_after = self.class.instance_variable_get '@methods_after'
+                if methods_before[name.to_s]
+                  methods_before[name.to_s].each do |method|
+                      self.send method
+                  end
+                end
+                  self.send "original_#{name}"
+                if methods_after[name.to_s]
+                  methods_after[name.to_s].each do |method|
+                      self.send method
+                  end
+                end
               end
+              alias_method name, "#{name}_new"
           end
         end
       end
     end
     def method_missing(meth, *args, &block)
-      @methods_before ||= {}
-      @methods_after ||= {}
       if meth.match /^before_/
-           method_name = meth.gsub /^before_/,''
-           @methods_before[method_name] ||= [] 
-           @methods_before[method_name].push *args 
+          unless instance_variable_defined "@methods_before"
+           instance_variable_set "@methods_before", Hash.new([])
+          end
+          method_name = meth.to_s.gsub /^before_/,''
+          instance_eval do
+              @methods_before[method_name] = args 
+          end
       end
       if meth.match /^after_/
-          method_name = meth.gsub /^after_/,''
-          @methods_after[method_name] ||= [] 
-          @methods_after[method_name].push *args 
+          unless instance_variable_defined "@methods_after"
+           instance_variable_set "@methods_after", Hash.new([])
+          end
+
+          method_name = meth.to_s.gsub /^after_/,''
+          instance_eval do 
+            @methods_after[method_name] = args 
+          end
       end
     end
-
   end
 end
 
 
 class Abc
   include Aop
-  before_method1 :method2, :method3
-  after_method2 :method1
+  after_method1 :method3
+  before_method1 :method2, :method3, :method3
+  after_method2 :method3
   def method1
     puts 'm1'
   end
@@ -50,5 +70,6 @@ class Abc
   end
 end
 
+z = Abc.new
+z.method1
 
-Abc.new
