068e092097aac71197c71f6929c21a322c80fccd
Vitaliy Bykovec <vbykovec@gmail.com>
2013-05-21 06:27:38 +0300

    not done yet

--- task1.rb
+++ task1.rb
@@ -1,11 +1,44 @@
+
 module Aop
   def self.included(mod)
-    puts mod
+    mod.extend RAop
+  end
+  module RAop
+    def self.extended(base)
+      base.instance_eval do
+        def method_added(name)
+          return if !@methods_before.keys.include? && @methods_after.keys.include? name 
+          class_eval do
+            unless method_defined?(:)
+              define_method(:) do
+              end
+          end
+        end
+      end
+    end
+    def method_missing(meth, *args, &block)
+      @methods_before ||= {}
+      @methods_after ||= {}
+      if meth.match /^before_/
+           method_name = meth.gsub /^before_/,''
+           @methods_before[method_name] ||= [] 
+           @methods_before[method_name].push *args 
+      end
+      if meth.match /^after_/
+          method_name = meth.gsub /^after_/,''
+          @methods_after[method_name] ||= [] 
+          @methods_after[method_name].push *args 
+      end
+    end
+
   end
 end
 
+
 class Abc
   include Aop
+  before_method1 :method2, :method3
+  after_method2 :method1
   def method1
     puts 'm1'
   end
@@ -16,3 +49,6 @@ class Abc
     puts 'm3'
   end
 end
+
+
+Abc.new
